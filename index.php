2a6bfd46cba62cf56558075187b1bfb5a2f21e6d
Alan Go <55218308+pandoralarm@users.noreply.github.com>
2022-08-25 17:24:58 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,378 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/adapts.css">
+
+    <!-- FONTS -->
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Source Sans Pro' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Source Code Pro' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Yatra One' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    
+    <style>
+      
+    </style>
+    <title>Its Alan's Box!</title>
+  </head>
+  <body>
+  
+  
+
+<div id="viewer" class="container-fluid">
+
+
+
+    <section id="intro" class="intro" style="height: 100vh; width: 100vw;">
+
+        <div style="height: 100vh;">
+            <canvas id="canvas-basic"></canvas>
+            
+
+            <div class="row intro-parts" style="height: 40vh;">
+                <div class="col d-flex text-center">
+                    <span style="font-size: 2rem; margin: 0; padding: 0;" id="typed" class="help m-auto"></span>
+                </div>
+            </div>
+
+            <div class="row intro-parts" style="height: 15vh;">
+                <div class="col"></div>
+
+                <div class="col-6 d-flex text-center" style="min-height: 50%; ">
+                    <span style="font-size: 2.5rem; margin: 0; padding: 0;" id="typed" class="element  m-auto"></span>
+
+                </div>
+                
+                <div class="col"></div>
+            </div>
+
+            <div class="row intro-parts" style="height: 10vh;">
+                <div class="col text-center">
+                    <div id="more" class="btn btn-outline-dark text-center">Find out More</div>
+                </div>
+            </div>
+
+            <div class="row intro-parts" style="height: 35vh;">
+                <div class="col"></div>
+            </div>
+        </div>
+
+
+
+    </section>
+
+    <section id="main" >
+
+
+        <div id="headliner" class="snappy" style="height: 100vh;">
+            <div id="headnav" class="row navs text-center sticky-top" style="height: 7vh; background-color: black;">
+                <div class="col d-block my-auto">
+                    <span>ALANSBOX</span>
+                </div>
+            </div>
+
+            <div  class="row" style="height: 63vh;">
+                <div class="col d-block m-auto text-center">
+                    <h1 class="pt-5 mx-auto">
+                    <span class="font-weight-light">Your website represents your great</span> <span id="typed-4" style="font-weight: 900;" class=".head"></span>
+                    </h1>
+                </div>
+            </div>
+
+            <div class="row" style="height: 30vh;">
+                <div class="col mock m-3">
+
+                </div>
+            </div>
+        </div>
+
+
+        <div id="profile" class="snappy" style="height: 100vh;">
+
+            <div id="pronav" class="row navs text-center sticky-top" style="height: 7vh;">
+                <div class="col d-block my-auto">
+                    <span>PROFILE</span>
+                </div>
+            </div>
+
+            <div class="row" style="height: 28vh;">
+                <div class="col my-auto">
+                    <div class="text-center paraghraph" style="font-size: 4rem; font-weight: 900;">HEADS UP!</div>
+                </div>
+            </div>
+
+
+            <div class="row paragraph "  style="height: 50vh;">
+                <div class="col-8 border m-auto px-4 littlebouts">
+                    <p class="mt-4">
+                        I'M ALAN, A 20 YEARS OLD GUY, BORN AND LIVES IN INDONESIA. <br />
+                        STARTED TINKERING ON COMPUTER SOFTWARE AND HARDWARE SINCE I WAS 11. <br />
+                        HIGH MOTIVATION AND CURIOSITY ABOUT DESIGNS AND WHAT CONNECTS US ON INTERNET <br />
+                        LED ME HERE CHASING MY PASSION AND DOING WHAT I LOVE.
+                    </p>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br /<=>
+                    <p class="text-right">
+                        I'M A WEB DEVELOPER, I WORK AS FRONT-END AND BACK-END DEVELOPER. <br />
+                        BESIDES WHAT I COULD DO, I'M ALSO PAVING MY WAY TO BE A GRAPHIC <br />
+                        AND UI/UX DESIGNER. CURRENTLY TAKING STUDY ON <a href="https://ipb.ac.id/faculty/index/vocational-school/"> IPB UNIVERSITY VOCATIONAL SCHOOL </a><br />
+                        HELPS A LOT IN MY EFFORT TO EXCEL AND MASTER ON THESE MATTERS.
+                    </p>
+                </div>
+            </div>
+
+            <div class="row" style="height: 10vh;">
+                <div clas="col">
+
+                </div>
+            </div>
+
+        </div>
+
+
+
+        <div id="projects" class="snappy" style="height: 365vh;">
+
+            <div id="tasknav" class="row navs text-center sticky-top" style="height: 7vh;">
+                <div class="col d-block my-auto">
+                    <span>RECENT PROJECTS</span>
+                </div>
+            </div>
+
+            <div class="row" style="height: 28vh;">
+                <div class="col my-auto">
+                    <div class="text-center paraghraph" style="font-size: 4rem; font-weight: 900;">RECENT PROJECTS</div>
+                    <div class="text-center paragraph text-black-50" style="font-size: 1rem; font-weight: lighter;">
+                        Here lies my recent projects I have worked on! <br />
+                        Well, most of 'em are part of college projects, though. <br />
+                        That's why I want US to work together!
+                    </div>
+                </div>
+            </div>
+
+            <div class="row" style="height: 65vh;">
+                <div class="col-7 anxwer">
+                    
+                </div>
+                <div class="col">
+                    <div class="descriptions" style="margin-top: 10%;">
+                        <div style="font-size: 4rem; font-weight: bold;">ANXWER</div>
+                        <div class="font-weight-light text-black-50">
+                            Anxwer is a website made for people who are currently facing<br />
+                            mental health issues, specifically depression and anxiety.<br />
+                            <br />
+                            The spark of the main idea is the increasing numbers of people<br />
+                            who suffers and going through such conditions during this pandemic.<br />
+                            <br />
+                            Anxwer is our submission on the "Olimpiade Sekolah Vokasi 2020"<br />
+                            and received the award as "<u>The Most Innovative Website</u>"".<br />
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+
+            
+            <div class="row" style="height: 65vh;">
+
+                <div class="col">
+                    <div class="descriptions text-right" style="margin-top: 10%;">
+                        <div style="font-size: 4rem; font-weight: bold;">KONCOTERNAK</div>
+                        <div class="font-weight-light text-black-50">
+                            Koncoternak is a website made as a pitch idea, an effort to solve<br />
+                            the obstacles of transactions made by farms, either its crops or animals<br />
+                            we offered and IoT solution to slove these problems.<br />
+                            <br />
+                            Set on Kawedusan Village, Central Java. Such farms have to do<br />
+                            their transaction traditionally as animal markets.<br />
+                            <br />
+                            Koncoternak is our pitch on HACKAVIDIA as a competition category<br />
+                            on ARKAVIDIA 6.0 held by ITB, where we are challenged to develop programs<br />
+                            in 24 hours to solve the aforementioned problems.<br />
+                        </div>
+                    </div>
+                   
+                </div>
+
+                <div class="col-7 konco">
+                    
+                </div>
+
+            </div>
+
+
+            <div class="row" style="height: 65vh;">
+                <div class="col-7 jline">
+                    
+                </div>
+                <div class="col">
+                    <div class="descriptions" style="margin-top: 10%;">
+                        <div style="font-size: 4rem; font-weight: bold;">JLINE-056</div>
+                        <div class="font-weight-light text-black-50">
+                            JLINE-056 is a web landing page design, used as a company profile.<br />
+                            This webpage is made as a collaboration project between INF55 in<br />
+                            in one of our courses and PT.PIN Indonesia.<br />
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+
+            
+            <div class="row" style="height: 65vh;">
+
+                <div class="col">
+                    <div class="descriptions text-right" style="margin-top: 10%;">
+                        <div style="font-size: 4rem; font-weight: bold;">GNFMART</div>
+                        <div class="font-weight-light text-black-50">
+                            GNFMart is a website mockup made for one of my courses in<br />
+                            college which to demonstrate the usage of Ajax on a web<br />
+                            application, and also demonstrate the business process of<br />
+                            an online shop, hence the mockup of the various admin panels<br />
+                            available in it.
+                            <br />
+                        </div>
+                    </div>
+                   
+                </div>
+
+                <div class="col-7 gnf">
+                    
+                </div>
+
+            </div>
+
+
+            <div class="row" style="height: 65vh;">
+                <div class="col-7 sitau">
+                    
+                </div>
+                <div class="col">
+                    <div class="descriptions" style="margin-top: 10%;">
+                        <div style="font-size: 4rem; font-weight: bold;">SITAU!</div>
+                        <div class="font-weight-light text-black-50">
+                            SiTau! is a simple web application made for one of my courses<br />
+                            in college, as a way for us to understand more about SDLC, its types,
+                            and each of its own differences betwee methodologies.<br />
+                            <br />
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+
+
+
+        </div>
+
+
+        <div id="profile" class="snappy" style="height: auto;">
+
+            <div id="pronav" class="row navs text-center sticky-top" style="height: 7vh;">
+                <div class="col d-block my-auto">
+                    <span>CONTACT</span>
+                </div>
+            </div>
+
+            <div class="row" style="height: 28vh;">
+                <div class="col my-auto">
+                    <div class="text-center paraghraph" style="font-size: 4rem; font-weight: 900;">LET'S COLLABORATE</div>
+                    <div class="text-center paragraph text-black-50" style="font-size: 1rem; font-weight: lighter;">
+                        Enought about me, tell me about yourself!
+                    </div>
+                </div>
+            </div>
+
+            <div class="row paragraph"  style="height: auto;">
+                <div class="col-8 border m-auto px-4 ">
+                    <!-- Default form contact -->
+                    <form class="text-center border border-light p-5" action="#!">
+
+                        <p class="h4 mb-4">Send me an email</p>
+
+                        <!-- Name -->
+                        <input type="text" id="defaultContactFormName" class="form-control mb-4" placeholder="Name">
+
+                        <!-- Email -->
+                        <input type="email" id="defaultContactFormEmail" class="form-control mb-4" placeholder="E-mail">
+
+                        <!-- Subject -->
+                        <label>Subject</label>
+                        <select class="browser-default custom-select mb-4">
+                            <option value="" disabled selected>Choose option</option>
+                            <option value="1" >Just saying hi!</option>
+                            <option value="2">Business Inquiry</option>
+                        </select>
+
+                        <!-- Message -->
+                        <div class="form-group">
+                            <textarea class="form-control rounded-0" id="exampleFormControlTextarea2" rows="3" placeholder="Message"></textarea>
+                        </div>
+
+                        <!-- Copy -->
+                        <div class="custom-control custom-checkbox mb-4">
+                            <input type="checkbox" class="custom-control-input" id="defaultContactFormCopy">
+                            <label class="custom-control-label" for="defaultContactFormCopy">Send me a copy of this message</label>
+                        </div>
+
+                        <!-- Send button -->
+                        <button class="btn btn-dark btn-block" type="submit">Send</button>
+
+                    </form>
+                    <!-- Default form contact -->
+                </div>
+            </div>
+
+            <div class="row" style="height: 10vh;">
+                <div class="col">
+                    &nbsp;
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+
+
+</div>
+
+
+
+  <section id="footer">
+    <div class="row text-white" style="height: 10vh; background-color: black;">
+      <div class="col-8 text-center m-auto">
+        <a href="www.twitter.com/pandoralarm">Twitter</a>
+        <div class="d-inline" style="width: 30%;">&nbsp;</div>
+        <a href="www.linkedin.com/in/alanraihan">Linkedin</a>
+      </div>
+    </div>
+    <div class="row text-white" style="height: 5vh; background-color: black;">
+      <div class="col-8 text-center m-auto">
+         <span style="font-weight: 200;">&copy; 2020 Copyright: </span> <strong>alansbox.online</strong>
+      </div>
+    </div>
+  </section>
+
+
+    <!-- BOOTSTRAP SCRIPSTS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-parallax-js@5.5.1/dist/simpleParallax.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
+    <script src="js/granim.min.js"></script>
+    <script src="js/adapts.js"></script>
+    
+  </body>
+</html>
